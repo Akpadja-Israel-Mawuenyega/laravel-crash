47c948d7d89478013b6f4e3042984db154a93096
Israel Akpadja <drimmaculate147@gmail.com>
2025-02-20 17:29:46 +0000

    seeded database and created note models and controller with nessecary migrations, added routes

--- app/Http/Controllers/welcomeController.php
+++ app/Http/Controllers/welcomeController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class welcomeController extends Controller
 {
     public function welcome()
     {
-        return view("welcome");
+        return view('welcome');
     }
 }
